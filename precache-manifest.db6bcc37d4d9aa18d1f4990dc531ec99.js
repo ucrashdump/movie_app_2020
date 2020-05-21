c5dd99d08ae04bd38e5abdaf7f1d72bbe3189882
Sangmin Lee <ucrashdump@rootfintech.com>
2020-05-21 17:12:29 +0900

    Updates

--- precache-manifest.db6bcc37d4d9aa18d1f4990dc531ec99.js
+++ precache-manifest.db6bcc37d4d9aa18d1f4990dc531ec99.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "edd8eb1faa100fd57c46fcbdedda2651",
+    "revision": "f6b6963232508990c1897196e31146ca",
     "url": "/movie_app_2020/index.html"
   },
   {
-    "revision": "aefa2f667b7528a2dce8",
-    "url": "/movie_app_2020/static/css/main.1b3d7f2e.chunk.css"
+    "revision": "d8e3628f5533e742b1e7",
+    "url": "/movie_app_2020/static/css/main.e03fb8c9.chunk.css"
   },
   {
     "revision": "79907e5e853fc1fa7794",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/movie_app_2020/static/js/2.0285f39f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "aefa2f667b7528a2dce8",
-    "url": "/movie_app_2020/static/js/main.4ea49cde.chunk.js"
+    "revision": "d8e3628f5533e742b1e7",
+    "url": "/movie_app_2020/static/js/main.06d6d573.chunk.js"
   },
   {
     "revision": "86e4940ccaeed0f37e85",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie_app_2020/precache-manifest.fad906b5e4fe2ae742c1cba54613891e.js"
+  "/movie_app_2020/precache-manifest.db6bcc37d4d9aa18d1f4990dc531ec99.js"
 );
 
 self.addEventListener('message', (event) => {
